98d23d4ed46f5f1c64acd9654cabbd99759f6835
ajayythakur <ajay1270panwar@gmail.com>
2022-12-17 01:50:14 +0530

    JS Handson 12(i)

---
+++ index.js
@@ -0,0 +1,11 @@
+function A(){
+    console.log("MY NAME IS AJAY");
+}
+A()
+
+function addition(num1,num2){
+let sum=0;
+sum=num1+num2;
+console.log(`Sum of ${num1},${num2} is ${sum}`)
+}
+addition(3,4)
